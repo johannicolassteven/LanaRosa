166ee47772cd2f1e1cae0d127e6fe0e7c4f158fa
johannicolassteven <nicosteveneg900@gmail.com>
2023-07-10 20:35:29 -0500

    single-product

--- db.jsx
+++ db.jsx
@@ -5,7 +5,10 @@ export const tops = [
       "color": "Negro",
       "precio": "55.000",
       "imagen1": "https://i.ibb.co/s6gThXB/Negro1.jpg",
-      "imagen2": "https://i.ibb.co/1RkTfhj/Negro2.jpg"
+      "imagen2": "https://i.ibb.co/1RkTfhj/Negro2.jpg",
+      "imagen3": "https://i.ibb.co/HTWrpGD/Lanas1.jpg",
+
+      
     },
 
     {
@@ -14,7 +17,9 @@ export const tops = [
       "color": "Blanco",
       "precio": "55.000",
       "imagen1": "https://i.ibb.co/zZHLqwF/Blanco1.jpg",
-      "imagen2": "https://i.ibb.co/TYhBkpS/Blanco2.jpg"
+      "imagen2": "https://i.ibb.co/TYhBkpS/Blanco2.jpg",
+      "imagen3": "https://i.ibb.co/HTWrpGD/Lanas1.jpg",
+
     },
     {
       "id": 3,
@@ -22,7 +27,9 @@ export const tops = [
       "color": "Rosado",
       "precio": "55.000",
       "imagen1": "https://i.ibb.co/qdYFSsQ/Rosado1.jpg",
-      "imagen2": "https://i.ibb.co/6F8T2Y1/Rosado2.jpg"
+      "imagen2": "https://i.ibb.co/6F8T2Y1/Rosado2.jpg",
+      "imagen3": "https://i.ibb.co/HTWrpGD/Lanas1.jpg",
+
     },
 
     {
@@ -31,7 +38,9 @@ export const tops = [
       "color": "Fuccia",
       "precio": "55.000",
       "imagen1": "https://i.ibb.co/WD8kWXm/Morado1.jpg",
-      "imagen2": "https://i.ibb.co/9r4wMZm/Morado2.jpg"
+      "imagen2": "https://i.ibb.co/9r4wMZm/Morado2.jpg",
+      "imagen3": "https://i.ibb.co/HTWrpGD/Lanas1.jpg",
+
     },
     {
       "id": 5,
@@ -39,7 +48,9 @@ export const tops = [
       "color": "Negro",
       "precio": "60.000",
       "imagen1": "https://i.ibb.co/fF9f7dL/Verde1.jpg",
-      "imagen2": "https://i.ibb.co/w6V2FDM/Verde2.jpg"
+      "imagen2": "https://i.ibb.co/w6V2FDM/Verde2.jpg",
+      "imagen3": "https://i.ibb.co/HTWrpGD/Lanas1.jpg",
+
     },
 
     {
@@ -48,7 +59,9 @@ export const tops = [
       "color": "Azul Claro",
       "precio": "50.000",
       "imagen1": "https://i.ibb.co/vc99SQY/Blanco11.jpg",
-      "imagen2": "https://i.ibb.co/NFZnHcv/Blanco-Top2.jpg"
+      "imagen2": "https://i.ibb.co/NFZnHcv/Blanco-Top2.jpg",
+      "imagen3": "https://i.ibb.co/HTWrpGD/Lanas1.jpg",
+
     },
   ]
 
--- src/Pages/SingleProductPage.jsx
+++ src/Pages/SingleProductPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { styled } from "styled-components";
 import { tops } from "../../db";
@@ -10,41 +10,60 @@ const SingleProduct = () => {
   return (
     <Wrapper>
       {tops.map((top) => {
-        var { id, name, imagen1, imagen2, precio, color } = top;
+        var { id, name, imagen1, imagen2,imagen3, precio, color } = top;
+        const [main, setMain] = useState(imagen1);
         if (id === parametro) {
           return (
             <div key={id}>
               <PageHero title={name} product></PageHero>
-                <div className="product-card">
-                  <div className="producto-image">
-                    <img src={imagen1} className="image" alt={name}></img>
-                    <img src={imagen2} className="image2" alt={name}></img>
-                  </div>
-                  <div className="descripcion">
-                    <h5 className="titulo">{name}</h5>
-                    <p className="price">${precio}</p>
-                    <div className="icons">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        fill="currentColor"
-                        className="bi bi-cart"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        fill="currentColor"
-                        className="bi bi-heart"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                      </svg>
-                    </div>
+              <div className="product-card">
+                <div className="producto-image">
+                  <Link to={main}>
+                    <img src={main} className="main" alt={name}></img>
+                  </Link>
+                  <div className="gallery">
+                  <img
+                      src={imagen1}
+                      alt="1"
+                      onClick={() => setMain(imagen1)}
+                    />
+                    <img
+                      src={imagen2}
+                      alt="2"
+                      onClick={() => setMain(imagen2)}
+                    />
+                    <img
+                      src={imagen3}
+                      alt="3"
+                      onClick={() => setMain(imagen3)}
+                    />
                   </div>
                 </div>
+                <div className="descripcion">
+                  <h5 className="titulo">{name}</h5>
+                  <p className="price">${precio}</p>
+                  <div className="icons">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      fill="currentColor"
+                      className="bi bi-cart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      fill="currentColor"
+                      className="bi bi-heart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
             </div>
           );
         }
@@ -55,22 +74,56 @@ const SingleProduct = () => {
 
 const Wrapper = styled.section`
 
-.product-card{
-  display: flex;
-  justify-content: center;
-  margin: 2rem 0;
-}
-
-  img {
+  .product-card {
     display: flex;
-    max-width: 20.5rem;
-    min-width: 1rem;
+    justify-content: center;
+    margin: 2rem 0;
+  }
+
+  .main{
+    display : flex;
+    max-width: 14rem;
+    object-fit: contain;
   }
 
   .image2 {
     display: none;
   }
 
+  .producto-image{
+
+  }
+
+  .gallery {
+    margin: .5rem auto 0;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2px;
+    img {
+      height: 95px;
+      cursor: pointer;
+      border: solid 1px black
+    }
+  }
+
+  .descripcion{
+    display: flex;
+    background-color:var(--cafe);
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    min-width: 8rem;
+    h5{
+      font-size: 1rem;
+    }
+    .icons{
+      display: flex;
+      flex-direction: column;
+      svg{
+        margin: 1rem 0;
+      }
+    }
+  }
 
   @media (min-width: 992px) {
   }
